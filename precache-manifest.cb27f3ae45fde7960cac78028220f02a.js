47f36cc70acb28a92adab7278bd64d9a83c40880
unknown <vetrivelkvk@outlook.com>
2020-09-05 11:04:37 +0530

    Updates

---
+++ precache-manifest.cb27f3ae45fde7960cac78028220f02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084731c05ba6c41b1ac14415c55afd0c",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "ffa06f6775545d69b5c7",
+    "url": "/sampleapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e0cef0a59a571ca56c6",
+    "url": "/sampleapp/static/js/2.a0f408f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sampleapp/static/js/2.a0f408f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa06f6775545d69b5c7",
+    "url": "/sampleapp/static/js/main.78cc8691.chunk.js"
+  },
+  {
+    "revision": "cb71b7c62be1a105e4fb",
+    "url": "/sampleapp/static/js/runtime-main.302daabd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
